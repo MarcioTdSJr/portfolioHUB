da4f7b35a7189847f5f0143ed1e43b164930dce2
MarcioTdSJr <101334563+MarcioTdSJr@users.noreply.github.com>
2026-05-14 21:57:20 -0300

    Create AES.java

---
+++ Seguranca/AES.java
@@ -0,0 +1,45 @@
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AES_IMPORTANTE {
+    private static String encriptar (String texto, String chave) throws Exception{
+        Cipher objCifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKey objChave = new SecretKeySpec(chave.getBytes("UTF-8"), "AES");
+        IvParameterSpec objIv = new IvParameterSpec ("0123456789101112" .getBytes());
+        objCifra.init(Cipher.ENCRYPT_MODE, objChave, objIv);
+        byte[] vetor = objCifra.doFinal(texto.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(vetor);
+    }   
+    private static String decriptar (String cifra, String chave) throws Exception{
+        Cipher objCifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKey objChave = new SecretKeySpec(chave.getBytes("UTF-8"), "AES");
+        IvParameterSpec objIv = new IvParameterSpec ("0123456789101112" .getBytes());
+        objCifra.init(Cipher.DECRYPT_MODE, objChave, objIv);
+        byte[] vetor = objCifra.doFinal(Base64.getDecoder().decode(cifra));
+            return new String(vetor, "UTF-8");
+            
+    }
+    public static void main(String[] args){
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+        
+        try{
+            System.out.print("Digite um texto: ");
+            String texto = leitor.readLine();
+            
+            System.out.print("Digite uma chave: ");
+            String chave = leitor.readLine();
+            
+            String cifra = encriptar(texto, chave);
+                
+                System.out.println(cifra);
+                System.out.println(decriptar(cifra,chave));
+        }catch (Exception erro){
+            System.out.println(erro);
+        }
+    }
+}
