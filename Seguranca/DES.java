5a765f6154c7055b2a46797d60c28e48d1d8fae0
MarcioTdSJr <101334563+MarcioTdSJr@users.noreply.github.com>
2026-05-14 21:52:30 -0300

    Update DES.java

--- Seguranca/DES.java
+++ Seguranca/DES.java
@@ -1,1 +1,41 @@
-
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Base64;
+                            //A chave obrigatoriamente tem que ter 24 caracteres e nao pode passar disso//
+public class DES {
+    private static String encriptar(String texto, String chave) throws Exception{
+        Cipher objCifra = Cipher.getInstance("DESede");
+        SecretKey objChave = new SecretKeySpec(chave.getBytes("UTF-8"), "DESede");
+        objCifra.init(Cipher.ENCRYPT_MODE, objChave);
+        byte[] vetor = objCifra.doFinal(texto.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(vetor);    
+    }    
+    private static String decriptar(String cifra, String chave) throws Exception{
+        Cipher objCifra = Cipher.getInstance("DESede");
+        SecretKey objChave = new SecretKeySpec(chave.getBytes("UTF-8"), "DESede");
+        objCifra.init(Cipher.DECRYPT_MODE, objChave);
+        byte[] vetor = objCifra.doFinal(Base64.getDecoder().decode(cifra));
+            return new String(vetor, "UTF-8");
+    }
+    public static void main(String[] args){
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+        
+        try{
+            System.out.print("Digite um texto: ");
+            String texto = leitor.readLine();
+            
+            System.out.print("Digite uma chave: ");
+            String chave = leitor.readLine();
+            
+            String cifra = encriptar(texto, chave);
+                
+                System.out.println(cifra);
+                System.out.println(decriptar(cifra,chave));
+        }catch (Exception erro){
+            System.out.println(erro);
+        }
+    }
+}
